8bb4d907796e2b6848ee83d6f142537d73698642
kennethowl <kennethowl00@gmail.com>
2020-10-25 22:54:37 -0600

    Multiples Constructores

--- Escuela/Alumnos.cs
+++ Escuela/Alumnos.cs
@@ -3,4 +3,11 @@ public class Alumno {
     public int Codigo { get; set; }
     public string  Nombre { get; set; }
     public int  Carnet { get; set; }
+
+    public Alumno(int codigo, string nombre, int carnet)
+    {
+        Codigo = codigo;
+        Nombre = nombre;
+        Carnet = carnet;
+    }
 }
--- Escuela/Program.cs
+++ Escuela/Program.cs
@@ -6,16 +6,9 @@ namespace Escuela
     {
         static void Main(string[] args)
         {
-            Alumno alumno1 = new Alumno();
-            alumno1.Codigo = 1;
-            alumno1.Nombre = "Kenneth";
-            alumno1.Carnet = 2222222;
-
-            Console.WriteLine (alumno1.Codigo + " " + alumno1.Nombre + " " + alumno1.Carnet);
-            
-            
-                
-            
+        
+            Alumno alumno = new Alumno(1, "Juanes", 034555);
+            Console.WriteLine(alumno.Codigo + " " + alumno.Nombre + " " + alumno.Carnet);
         }
     }
 }
